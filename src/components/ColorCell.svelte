f595461aae9df213e7cd255a9dca785bfe05d1d2
Distrusttt <51008939+Distrusttt@users.noreply.github.com>
2022-04-05 10:31:44 -0400

    Create ColorCell.svelte

---
+++ src/components/ColorCell.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let props;
+</script>
+
+<div class="color-cell" style={"background: " + props.color}>
+    {#if props.icon}
+        <i class="fa-solid fa-circle-plus" style="color: red;"></i>
+    {/if}
+    {#if props.text}
+        <span>{props.text}</span>
+    {/if}
+</div>
