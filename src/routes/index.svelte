c82f8afbb70958ba3d9ff6912c00ceb2fefbf169
Distrusttt <51008939+Distrusttt@users.noreply.github.com>
2022-04-05 10:42:56 -0400

    spaces > tabs smh

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,13 +1,14 @@
 <script>
-	import ColorGrid from "./ColorGrid.svelte";
-	const colors = [
-			"#F84464", "#F844D9",
-			"#C744F8", "#9244F8",
-			"#5844F8", "#449cf8",
-			"#44F8C3", "#44F863",
-			"#DEF844", "#F89744",
-			"#F86344"
-	];
+    import ColorGrid from "./ColorGrid.svelte";
+
+    const colors = [
+        "#F84464", "#F844D9",
+        "#C744F8", "#9244F8",
+        "#5844F8", "#449cf8",
+        "#44F8C3", "#44F863",
+        "#DEF844", "#F89744",
+        "#F86344"
+    ];
 </script>
 
 <ColorGrid {colors} />
