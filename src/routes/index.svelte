55acaecfb81ff12ee92a466155eabfae19080189
Distrusttt <51008939+Distrusttt@users.noreply.github.com>
2022-04-05 10:42:11 -0400

    color grid go brrrrrr

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,13 @@
-<h1>MK Chat Client v3??????</h1>
+<script>
+	import ColorGrid from "./ColorGrid.svelte";
+	const colors = [
+			"#F84464", "#F844D9",
+			"#C744F8", "#9244F8",
+			"#5844F8", "#449cf8",
+			"#44F8C3", "#44F863",
+			"#DEF844", "#F89744",
+			"#F86344"
+	];
+</script>
+
+<ColorGrid {colors} />
