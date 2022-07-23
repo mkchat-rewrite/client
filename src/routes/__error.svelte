c265f2ee1e8a0047d005fbd89707a1d6898c4d9f
Distrusttt <51008939+Distrusttt@users.noreply.github.com>
2022-07-23 06:03:41 -0400

    error page goes hard

---
+++ src/routes/__error.svelte
@@ -0,0 +1,47 @@
+<script lang="ts" context="module">
+    export const load = (data: Record<string, any>) => {
+        return {
+            props: {
+                title: data.status,
+                description: data.error?.message
+            }
+        };
+    };
+</script>
+
+<script lang="ts">
+    import { onMount } from "svelte";
+
+    export let title: string;
+    export let description: string;
+
+    const colors = [
+        "slateblue",
+        "skyblue",
+        "darkseagreen",
+        "indigo",
+        "lightsteelblue",
+        "tomato",
+        "mediumaquamarine",
+        "olivedrab",
+        "palevioletred",
+        "plum",
+        "yellowgreen",
+        "tan",
+        "teal"
+    ];
+
+    onMount(() => {
+        const color = colors[Math.floor(Math.random() * colors.length)];
+        const hueBg = !!Math.round(Math.random());
+
+        if (hueBg) {
+            document.body.style.backgroundColor = color;
+        } else {
+            document.body.style.color = color;
+        };
+    });
+</script>
+  
+<h1>{title}</h1>
+<pre>{description}</pre>
