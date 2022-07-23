88984297a0320b8e01e9acf6c9e8f77d627d7266
Distrusttt <51008939+Distrusttt@users.noreply.github.com>
2022-07-22 21:43:28 -0400

    :pear:

---
+++ src/components/TextInput.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+    export let placeholder = "";
+    let isFocus = false;
+
+    const handleFocus = (event: FocusEvent) => {
+        const element = event.target as HTMLInputElement;
+
+        element.placeholder = isFocus ? placeholder : "";
+        isFocus = !isFocus;
+    };
+</script>
+
+<span>
+    <span class="label" hidden={!isFocus}>{placeholder}</span>
+    <input type="text" placeholder={placeholder} on:focus={handleFocus} on:blur={handleFocus} />
+</span>
+
+<style>
+    span {
+        position: relative;
+    }
+
+    input {
+        width: 100%;
+    }
+
+    .label {
+        color: rgb(187, 189, 194);
+        position: absolute;
+        top: -1.5em;
+        font-size: .9em;
+    }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,26 @@
 <script>
     import Counter from "../components/Counter.svelte";
+    import TextInput from "../components/TextInput.svelte";
 </script>
 
 <main>
-    <h1>MK Chat Beta</h1>
+    <!-- <h1>MK Chat Beta</h1>
 
     <div class="card">
         <Counter />
+    </div> -->
+
+    <div class="form">
+        <span>
+            <h2>Welcome to MK Chat</h2>
+            <h6 id="motd">Room rp is more active</h6>
+        </span>
+        <TextInput placeholder="Nickname" />
+        <TextInput placeholder="Room" />
+        <button>Join</button>
     </div>
     
-    <p>
+    <!-- <p>
         Click <a href="https://mkchat.app" target="_blank">here</a> to return to the regular mkchat homepage.
     </p>
 
@@ -19,5 +30,5 @@
         <a href="https://mkchat.app/chat.html?room=test&name=🧶" target="_blank">Test Room</a>
         -
         <a href="https://rail-proxy.mkchat.app/discord/stickers/781323628267962408.png" target="_blank">Railway Proxy Demo URL</a>
-    </span>
+    </span> -->
 </main>
