739217328685476d7e1937ed66169766c5b0b226
Distrusttt <51008939+Distrusttt@users.noreply.github.com>
2022-07-22 22:25:35 -0400

    Update TextInput.svelte

--- src/components/TextInput.svelte
+++ src/components/TextInput.svelte
@@ -11,14 +11,21 @@
     const handleFocus = (event: FocusEvent) => {
         const element = event.target as HTMLInputElement;
 
+        if (value) return;
+
         element.placeholder = isFocus ? placeholder : "";
         isFocus = !isFocus;
+    };
+
+    const onchange = (event: InputEventInit) => { // idk why typescript wants this to be InputEventInit, but recasting below fixes so whatever
+        const element = (event as InputEvent).target as HTMLInputElement;
+        value = element.value;
     };
 </script>
 
 <span>
     <span class="label" hidden={!isFocus}>{placeholder}</span>
-    <input type="text" value={value} name={name} placeholder={placeholder} autocomplete={autocomplete} maxlength={maxlength} required={required} on:focus={handleFocus} on:blur={handleFocus} on:load={onload} />
+    <input type="text" value={value} name={name} placeholder={placeholder} autocomplete={autocomplete} maxlength={maxlength} required={required} on:focus={handleFocus} on:blur={handleFocus} on:input={onchange} />
 </span>
 
 <style>
