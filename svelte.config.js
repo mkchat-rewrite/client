f3c8c3c337fd535b4cba9c5daff07d7a0b4c4af4
Distrusttt <51008939+Distrusttt@users.noreply.github.com>
2022-08-16 12:17:50 -0400

    dsf98usdujkfhgwsedkjfghjsd imma kms

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,28 @@
-import adapter from "@sveltejs/adapter-node";
+// import adapter from "@sveltejs/adapter-node";
+import adapter from "@sveltejs/adapter-static";
 import preprocess from "svelte-preprocess";
 
 /** @type {import("@sveltejs/kit").Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+    // Consult https://github.com/sveltejs/svelte-preprocess
+    // for more information about preprocessors
+    preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter(),
+    kit: {
+        adapter: adapter({
+            // default options are shown. On some platforms
+            // these options are set automatically — see below
+            pages: 'build',
+            assets: 'build',
+            fallback: null,
+            precompress: false
+        }),
+
         prerender: {
-            enabled: false
+            // This can be false if you're using a fallback (i.e. SPA mode)
+            default: true
         }
-	}
+    }
 };
 
 export default config;
