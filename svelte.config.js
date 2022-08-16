4d3b6c996d5afefdd8e49f0087961b4905e5c6c1
Distrusttt <51008939+Distrusttt@users.noreply.github.com>
2022-08-16 12:29:44 -0400

    bru

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-// import adapter from "@sveltejs/adapter-node";
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-node";
+// import adapter from "@sveltejs/adapter-static";
 import preprocess from "svelte-preprocess";
 
 /** @type {import("@sveltejs/kit").Config} */
@@ -9,19 +9,12 @@ const config = {
     preprocess: preprocess(),
 
     kit: {
-        adapter: adapter({
-            // default options are shown. On some platforms
-            // these options are set automatically — see below
-            pages: 'build',
-            assets: 'build',
-            fallback: null,
-            precompress: false
-        }),
+        adapter: adapter(),
 
-        prerender: {
-            // This can be false if you're using a fallback (i.e. SPA mode)
-            default: true
-        }
+        // prerender: {
+        //     // This can be false if you're using a fallback (i.e. SPA mode)
+        //     default: true
+        // }
     }
 };
 
