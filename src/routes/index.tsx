8d1c4d1e0f67a1cdba2e365c3e19b0e8ce3ba8cd
Distrusttt <51008939+Distrusttt@users.noreply.github.com>
2022-10-08 12:30:19 -0400

    t

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,22 +4,34 @@ import { Link } from "@builder.io/qwik-city";
 
 export default component$(() => {
     return (
-        <div>
-            <h1 class="text-3xl font-bold underline">
-                Hello world!
-            </h1>
+        <>
+            <div class="h-24 flex items-center justify-between p-8">
+                <Link href="/"><h1 class="text-white">[LOGO]</h1></Link>
 
-            <button type="submit" class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                <div class="flex gap-3">
+                    <Link href="/about"><button class="rounded-md py-2 px-4 text-white/60 hover:underline">About</button></Link>
+                    <Link href="/rules"><button class="rounded-md py-2 px-4 text-white/60 hover:underline">Rules</button></Link>
+                    <Link href="/discord"><button class="rounded-md py-2 px-4 text-white/60 hover:underline">Discord Server</button></Link>
+                </div>
+
+
+                <div class="flex gap-4">
+                    <button class="rounded-md py-2 px-4 text-white/60 hover:underline">Login</button>
+                    <button class="bg-indigo-600/10 rounded-md py-2 px-4 text-white/80 border-1 border-indigo-600/80 hover:bg-indigo-600">Signup</button>
+                </div>
+            </div>
+
+            {/* <button type="submit" class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
                 Sign in
             </button>
 
             <Link class="mindblow" href="/flower">
                 Blow my mind 🤯
-            </Link>
-        </div>
+            </Link> */}
+        </>
     );
 });
 
 export const head: DocumentHead = {
-    title: "Welcome to Qwik",
+    title: "[TBD]",
 };
--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -9,9 +9,7 @@ export default component$(() => {
                 </section>
             </main>
             <footer>
-                <a href="https://www.builder.io/" target="_blank">
-                    Made with ♡ by Builder.io
-                </a>
+
             </footer>
         </>
     );
