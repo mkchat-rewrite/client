dbd178f1c935427457d8dc7ae4682450488c3a9b
Distrusttt <51008939+Distrusttt@users.noreply.github.com>
2022-10-29 20:17:20 -0400

    small form changes

--- src/routes/register/index.tsx
+++ src/routes/register/index.tsx
@@ -5,32 +5,30 @@ import { GoogleLogo } from "~/components/icons/google";
 import { DiscordLogo } from "~/components/icons/discord";
 
 export default component$(() => {
+    async function handleFormSubmit(event: Event) {
+        const target = event.target as HTMLFormElement;
+        const data = new FormData(event.target as HTMLFormElement);
+        const formData = Array.from(data.entries()).map((e) => e.join("=")).join("&");
+
+        await fetch(target.action, {
+            method: target.method,
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+            body: formData
+        });
+    };
+
     return (
         <div class="w-screen h-screen grid place-items-center">
             <div class="grid w-96 gap-5">
-                <form class="grid gap-3" action="https://api.mkchat.app/auth/account/register" method="POST" preventDefault:submit onSubmit$={async (event) => {
-                    const target = event.target as HTMLFormElement;
-                    const data = new FormData(event.target as HTMLFormElement);
-                    const formData = Array.from(data.entries()).map((e) => e.join("=")).join("&");
-
-                    await fetch(target.action, {
-                        method: target.method,
-                        headers: {
-                            "Content-Type": "application/x-www-form-urlencoded",
-                        },
-                        body: formData
-                    }).then(() => {
-                        console.log("success");
-                    }).catch(() => {
-                        console.log("failed");
-                    });
-                }}>
+                <form class="grid gap-3" action="https://api.mkchat.app/auth/account/register" method="POST" preventDefault:submit onSubmit$={handleFormSubmit}>
                     <div class="flex gap-2 mb-1">
-                        <button type="submit" class="flex justify-center items-center gap-2 rounded-md py-2 px-3 w-full bg-black/50 text-white">
+                        <button class="flex justify-center items-center gap-2 rounded-md py-2 px-3 w-full bg-black/50 text-white">
                             <GoogleLogo />
                             <span>Google</span>
                         </button>
-                        <button type="submit" class="flex justify-center items-center gap-2 rounded-md py-2 px-3 w-full bg-black/50 text-white">
+                        <button class="flex justify-center items-center gap-2 rounded-md py-2 px-3 w-full bg-black/50 text-white">
                             <DiscordLogo />
                             <span>Discord</span>
                         </button>
